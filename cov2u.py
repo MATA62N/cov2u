a0b38dd47fcd5597937dc94f175eba155bdc8405
Michael Taylor <patternizer@gmail.com>
2019-07-15 01:23:32 +0100

    Code tidy-up

--- cov2u.py
+++ cov2u.py
@@ -5,8 +5,8 @@
 # EXAMPLE:  python cov2u.py 37 cov.nc 2000
 
 # =======================================
-# Version 0.1
-# 20 June, 2019
+# Version 0.2
+# 25 June, 2019
 # https://patternizer.github.io
 # michael.taylor AT reading DOT ac DOT uk
 # =======================================
@@ -15,28 +15,30 @@ from  optparse import OptionParser
 import numpy as np
 import scipy.linalg as la
 import xarray
+import matplotlib.pyplot as plt
 
 def calc_cov2u(N, Xmean, Xcov):
     '''
     Routine to estimate uncertainty from a covariance matrix using Monte Carlo sampling from the underlying distribution. Code adapted from routine coded by Jonathan Mittaz: get_harm.py
     '''
 
-    Xmean = np.array([0.,0.])
-    Xcov = np.array([[0.04, 0.112] , [0.112, 0.64]])
+#    Xmean = np.array([0.,0.])
+#    Xcov = np.array([[0.04, 0.112] , [0.112, 0.64]])
     
-    uncert = np.array([[0.2,0.],[0.,0.8]])
+#    uncert = np.array([[0.2,0.],[0.,0.8]])
     # correl = np.array([[1.,0.7],[0.7,1.]])
     # cov = np.matmul(uncert,correl)
     # orig_cov = np.matmul(cov,uncert)
     # Xcov = np.copy(orig_cov)
-    print('U(target)=', uncert)
+#    print('U(target)=', uncert)
 
     eigenval, eigenvec = np.linalg.eig(Xcov)
     R = eigenvec
     S = np.diag(np.sqrt(eigenval))
     T = np.matmul(R,S)
     output = np.random.multivariate_normal(Xmean, Xcov, size=N)
-    ndims = len(Xcov.shape)
+#    ndims = len(Xcov.shape)
+    ndims = Xcov.shape[1]
     position = np.zeros((N, ndims))
     final = np.zeros((N, ndims))
     for j in range(ndims):
@@ -68,6 +70,8 @@ if __name__ == "__main__":
     ds = xarray.open_dataset(cov_file)
     Xave = ds.parameter
     Xcov = ds.parameter_covariance_matrix
+    Xcor = ds.parameter_correlation_matrix
+    Xu = ds.parameter_uncertainty
     U = calc_cov2u(N, Xave, Xcov)
     
     print('U(estimate)=', np.diag(U))
